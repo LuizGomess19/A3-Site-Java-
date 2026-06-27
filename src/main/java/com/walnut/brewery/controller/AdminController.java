8549656bdef27e99529e8b6fba20d358245421a7
leonardooalvsdev <124221849@uilife.com.br>
2026-06-27 00:18:02 -0300

    Melhorias A3 Leonardo

--- src/main/java/com/walnut/brewery/controller/AdminController.java
+++ src/main/java/com/walnut/brewery/controller/AdminController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class AdminController {
@@ -28,18 +30,48 @@ public class AdminController {
         double totalFaturamento = 0.0;
         int barris30 = 0;
         int barris50 = 0;
+        int litrosVendidos = 0;
+        int entregas = 0;
+        int retiradas = 0;
+
+Map<String, Integer> vendasPorCerveja = new HashMap<String, Integer>();
+
 
         for (Pedido p : pedidos) {
             totalFaturamento += p.getValorTotal();
             if (p.getTamanho() != null) {
                 if (p.getTamanho() == 30) {
                     barris30++;
+                    if (p.getTamanho() != null) {
+    litrosVendidos += p.getTamanho();
+}
+
+if ("entrega".equalsIgnoreCase(p.getTipoFrete())) {
+    entregas++;
+} else {
+    retiradas++;
+}
+
+String cerveja = p.getCerveja().toLowerCase();
+vendasPorCerveja.put(
+        cerveja,
+        vendasPorCerveja.getOrDefault(cerveja, 0) + 1
+);
                 } else if (p.getTamanho() == 50) {
                     barris50++;
                 }
             }
         }
+String cervejaMaisVendida = "Nenhuma";
+int maiorQuantidade = 0;
 
+for (Map.Entry<String, Integer> entry : vendasPorCerveja.entrySet()) {
+
+    if (entry.getValue() > maiorQuantidade) {
+        maiorQuantidade = entry.getValue();
+        cervejaMaisVendida = entry.getKey();
+    }
+}
         // manda as variaveis pro thymeleaf
 
         model.addAttribute("pedidos", pedidos);
@@ -49,7 +81,10 @@ public class AdminController {
         model.addAttribute("barris50", barris50);
 
         // chama o template admin.html
-
+        model.addAttribute("litrosVendidos", litrosVendidos);
+        model.addAttribute("entregas", entregas);
+        model.addAttribute("retiradas", retiradas);
+        model.addAttribute("cervejaMaisVendida", cervejaMaisVendida);
         return "admin";
     }
 }
